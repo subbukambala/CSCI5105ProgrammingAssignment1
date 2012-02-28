51025f987dce09acf2be7f59c66fb3fcea180ef3
Daniel DaCosta <chaosape@gmail.com>
2012-02-28 12:48:31 -0600

    Classpath being modified in support.sh now.

--- scripts/support.sh
+++ scripts/support.sh
@@ -2,6 +2,7 @@ t=`dirname ${rfile}`
 fqfiledir=`(cd $t && pwd)`
 fqjardir=${fqfiledir}/../jars/
 fqsrcdir=${fqfiledir}/../src/
+CLASSPATH=${CLASSPATH}:.:${fqsrcdir}:${fqjardir}commons-cli-1.2.jar
 function startRMIRegistry ()
 {
     rmiregrunningP=`ps aux | grep rmiregistry | grep -v grep`
