a4b696d486e5dd5bbcb8b74d95048495439b11a1
Daniel DaCosta <chaosape@gmail.com>
2012-02-29 16:50:18 -0600

    Rolled all client operations in CLI(Command Line Interface). Removed Client as it is now redundant. Added ArgumentHandler to make life dealing with command line arguments a bit easier.

---
+++ src/ArgumentHandler.java
@@ -0,0 +1,60 @@
+/**
+ * @authors Bala Subrahmanyam Kambala, Daniel William DaCosta
+ * @license GPLv3 (http://www.gnu.org/copyleft/gpl.html)
+ * @descriptrion Implements the Super Peer Interface.
+ */
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+
+public class ArgumentHandler 
+{
+
+    Options options;
+    String syntax;
+    String header;
+    String footer;
+    public ArgumentHandler(String _syntax,String _header, String _footer) 
+    {
+	options = new Options( );
+	syntax = _syntax;
+	header = _header;
+	footer = _footer;
+    }
+
+
+    public void addOption(String a,String b,boolean c,String d)
+    {
+	options.addOption(a,b,c,d);
+    }
+
+    public CommandLine parse(String [] args)
+    {
+        CommandLine commandLine;
+	try 
+	{
+	    CommandLineParser parser = new BasicParser();
+	    commandLine = parser.parse( options, args );
+	    return commandLine;
+	}
+	catch (Exception e)
+	{
+	    usage("\nImproper Usage!\n");
+	    System.exit(1);
+	    return null;
+        }
+    }
+
+
+    public void usage(String msg)
+    {
+	HelpFormatter helpFormatter = new HelpFormatter( );
+	helpFormatter.setWidth( 80 );
+	helpFormatter.printHelp(syntax,msg+header,options,footer);
+    }
+
+}
--- src/CLI.java
+++ src/CLI.java
@@ -3,6 +3,13 @@
  * @license GPLv3 (http://www.gnu.org/copyleft/gpl.html)
  * @descriptrion Implements the Super Peer Interface.
  */
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.logging.Level;
@@ -19,64 +26,109 @@ import org.apache.commons.cli.HelpFormatter;
  */
 public class CLI {
 
-   
-    public CLI () {}
     /**
      * @todo Everything
      */
     public static void main (String[] argv) 
     {
-	//Setup command line options
-	CommandLineParser parser = new BasicParser( );
-	Options options = new Options( );
-	options.addOption("h", "help", false, "Print this usage information.");
-	options.addOption("l", "listpeers", false, "List peers");
+	ArgumentHandler cli = new ArgumentHandler
+	    (
+	     "CLI [-hL] [-l word] [-f file] peer/super"
+	     ,"CLI provides remote access to peers and superpeers within this CHORD implementation."
+	     ,"Bala Subrahmanyam Kambala, Daniel William DaCosta - GPLv3 (http://www.gnu.org/copyleft/gpl.html)"
+	     );
+	cli.addOption("h", "help", false, "Print this usage information.");
+	cli.addOption("L", "listpeers", false, "Calls the RMI getPeer on the provided host. It will list all known peers.");
+	cli.addOption("l", "lookup", true, "Lookup a word from a particular peer.");
+	cli.addOption("f", "filename", true, "Load all definitions from a file from a particular peer.");
 
-	// Parse the program arguments
-	CommandLine commandLine;
-	try 
-        {
-	    commandLine = parser.parse( options, argv );
-	    if( commandLine.hasOption('h') ) {
-		HelpFormatter helpFormatter = new HelpFormatter( );
-		helpFormatter.setWidth( 80 );
-		helpFormatter.printHelp( "CLI Usage:", "", options, "");
-		System.exit(0);
-	    }
-	    if( commandLine.hasOption('l') ) {
-		try 
-		{
-		    
-		    // Find the SuperPeer
-		    SuperPeerInterface superpeer = (SuperPeerInterface) Naming.lookup("//" + commandLine.getArgs()[0] + "/SuperPeer");
-		    String[][] rv = superpeer.getPeers();
-		    for(int i = 0 ; i < rv.length ; i++ )
-		    {
-			System.out.println(Integer.toString(i)+") "+rv[i][0]+" = "+rv[i][1]);
-		    }
-		} catch (Exception e) 
-		{
-		    System.out.println ("SuperPeer failed: " + e);
-		}
-	    }
-	    else
-	    {
-		System.out.println( "Improper Usage" );
-		HelpFormatter helpFormatter = new HelpFormatter( );
-		helpFormatter.setWidth( 80 );
-		helpFormatter.printHelp( "SuperPeer Usage:", "", options, "");
-		System.exit(1);
-	    }
+	CommandLine commandLine = cli.parse(argv);
+	if( commandLine.hasOption('h') ) {
+	    cli.usage("");
+	    System.exit(0);
 	}
-        catch( Exception e ) 
-	{
-	    System.out.println( "Improper Usage" );
-	    HelpFormatter helpFormatter = new HelpFormatter( );
-	    helpFormatter.setWidth( 80 );
-	    helpFormatter.printHelp( "SuperPeer Usage:", "", options, "");
+
+	if( commandLine.getArgs().length == 0) {
+	    cli.usage("\nNo peer provided!\n");
 	    System.exit(1);
 	}
+
+	if( commandLine.hasOption('L') ) {
+	    try {   
+		// Find the SuperPeer
+		SuperPeerInterface superpeer = (SuperPeerInterface) Naming.lookup("//" + commandLine.getArgs()[0] + "/SuperPeer");
+		String[][] rv = superpeer.getPeers();
+		for(int i = 0 ; i < rv.length ; i++ ) {
+		    System.out.println(Integer.toString(i)+") "+rv[i][0]+" = "+rv[i][1]);
+		}
+	    } catch (Exception e) {
+		System.out.println ("SuperPeer failed: " + e);
+		System.exit(1);
+	    }
+	    System.exit(0);
+	}
+	if( commandLine.hasOption('l') ) {
+	    String word = commandLine.getOptionValue('l');
+	    if ( word == null ) {
+		cli.usage("\nNo word provided!\n");
+		System.exit(1);
+	    }
+	    // call lookup RMI service to find a meaning of word.
+	    System.out.println(word + "...word");
+	    System.exit(0);
+	}
+	if( commandLine.hasOption('f') ) {
+	    String filePath = commandLine.getOptionValue( 'f' );
+	    if (filePath == null ) {
+		cli.usage("\nNo file provided!\n");
+		System.exit(1);
+	    }
+		
+	    // XXX:: call lookup RMI service to find a meaning of a word.
+	    System.out.println(filePath + "... file path");
+	    Map<String, String> dict = null;
+	    try {
+		dict = CLI.readFile(filePath);
+	    } catch (Exception e) {
+		cli.usage("\nError opening file!\n");
+		System.exit(1);
+	    }
+
+	    Iterator it = dict.entrySet().iterator();
+	    while (it.hasNext()) {
+		Map.Entry pairs = (Map.Entry)it.next();
+		System.out.println(pairs.getKey() + " = " + pairs.getValue());
+		// XXX::  call insert RMI service to insert a meaning of a word
+	    }
+	    System.exit(0);
+	}
+	
+	cli.usage("\nNo directive provided!\n");
+	System.exit(1);
+	
+	
     }
 
+    public static Map<String, String> readFile(String fileName) throws Exception {
+
+	Map<String, String> dict = new HashMap<String, String> (); 
+	// Open the file that is the first
+	// command line parameter
+	FileInputStream fstream = new FileInputStream(fileName);
+	// Get the object of DataInputStream
+	DataInputStream in = new DataInputStream(fstream);
+	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	String strLine;
+	// Read File Line By Line
+	while ((strLine = br.readLine()) != null) {
+	    // Print the content on the console
+	    String words[] = strLine.split(":");
+	    dict.put(words[0].trim(), words[1].trim());
+	    System.out.println(strLine);
+	}
+	// Close the input stream
+	in.close();
+	return dict;
+    }
 
 }
